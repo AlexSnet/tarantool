d01fc15700b92794882cd7d867c6c3c58e58f304
Grechishkin Alexey <a.grechishkin@rambler-co.ru>
2015-07-07 16:22:40 +0300

    I think it will fix Tarantool on FreeBSD

--- third_party/libev/ev.c
+++ third_party/libev/ev.c
@@ -928,12 +928,12 @@ ecb_inline uint64_t ecb_rotr64 (uint64_t x, unsigned int count) { return (x << (
   }
 #endif
 
-#if ECB_GCC_VERSION(4,5)
+#if ECB_GCC_VERSION(4,5) || defined __clang__
   #define ecb_unreachable() __builtin_unreachable ()
 #else
   /* this seems to work fine, but gcc always emits a warning for it :/ */
-  ecb_inline void ecb_unreachable (void) ecb_noreturn;
-  ecb_inline void ecb_unreachable (void) { }
+  ecb_inline ecb_noreturn void ecb_unreachable (void);
+  ecb_inline ecb_noreturn void ecb_unreachable (void) { }
 #endif
 
 /* try to tell the compiler that some condition is definitely true */
